c64159a91665cf7b6d705d65adf1bdb8a1d54030
karthikvch <karthik998.ch@gmail.com>
2026-02-26 23:03:46 +0530

    changes v2

--- s3/variables.tf
+++ s3/variables.tf
@@ -1,4 +1,17 @@
-variable "bucket_name" { type = string }
-variable "acl"         { type = string }
-variable "versioning"  { type = bool  default = true }
-variable "force_destroy" { type = bool default = false }
+variable "bucket_name" {
+  type = string
+}
+
+variable "acl" {
+  type = string
+}
+
+variable "versioning" {
+  type    = bool
+  default = true
+}
+
+variable "force_destroy" {
+  type    = bool
+  default = false
+}
